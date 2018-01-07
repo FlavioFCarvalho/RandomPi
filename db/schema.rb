58af5716cc96b8e91bd15808f66026b5e42755bc
Flavio Carvalho <flaviocfr@gmail.com>
2018-01-07 00:53:44 -0200

    Add o postgresql o database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20180105191037) do
 
-  create_table "pics", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "pics", force: :cascade do |t|
     t.string   "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
