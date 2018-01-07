c6288049d385130bfa19384cfbe5ce7d1476ae52
Flavio Carvalho <flaviocfr@gmail.com>
2018-01-07 01:54:58 -0200

    Config.  o application.rb

--- config/application.rb
+++ config/application.rb
@@ -17,14 +17,9 @@ require "rails/test_unit/railtie"
 Bundler.require(*Rails.groups)
 
 module RandomPic
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+  class Application < Rails::Application # Settings in config/environments/* take precedence over those specified here. # Application configuration should go into files in config/initializers # -- all .rb files in that directory are automatically loaded. # Only loads a smaller set of middleware suitable for API only apps. # Middleware like session, flash, cookies can be added back manually. # Skip views, helpers and assets when generating a new resource. config.middleware.insert_before 0, Rack::Cors do allow do origins '*' resource '*', :headers => :any, :methods => [:get, :post, :options, :create]
+      end
+    end
     config.api_only = true
   end
 end
